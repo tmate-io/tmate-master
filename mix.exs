ec6a5e5a393c219138b3e43a960676dcc3664abd
Nicolas Viennot <nicolas@viennot.biz>
2016-04-06 12:39:26 -0400

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Tmate.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 1.0.2"},
+    [{:phoenix, "~> 1.1"},
      {:phoenix_ecto, "~> 1.1"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
